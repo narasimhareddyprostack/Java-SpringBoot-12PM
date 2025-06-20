f6f6ff86886e98962fbbd635d4876b496ab855fc
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-06-20 12:05:59 +0530

    4pillers

---
+++ OOP-6-4C/4pillers/src/AccService.java
@@ -0,0 +1,8 @@
+public class AccService {
+    public static  void getService(Account a){
+        a.cal_Bal();
+    }
+}
+
+//Account a - acting as many form
+//Account a - sometime acting SA objet,CA object
